e1990a0f7c37f0efa4c5defd1a98dff65686641f
rizmiya-ameen <rizmiyaamin@gmail.com>
2023-11-03 18:56:53 +1100

    useReducer multiple states

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import AddCounter from './components/AddCounter';
 import FullCounter from './components/FullCounter';
 import ToDo from './components/ToDo';
+import One from './components/useReducer4';
 import Counter from './components/useState';
 
 function App() {
   return (
     <div className="App">
+      <One />
       <ToDo />
       <Counter />
       <AddCounter />
